3aafb22f9277af02632184df8de1f864e2bf81fc
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2007-12-03 19:23:53 +0000

    mdek: - started xml schema file (.xsd) describing data structure of mdek requests and results - adapted Keys according to schema - improved mapping of beans to docs - refactoring

--- ingrid-mdek-services/src/main/java/de/ingrid/mdek/services/persistence/db/model/BeanToDocMapper.java
+++ ingrid-mdek-services/src/main/java/de/ingrid/mdek/services/persistence/db/model/BeanToDocMapper.java
@@ -11,6 +11,21 @@ import de.ingrid.utils.IngridDocument;
 public class BeanToDocMapper {
 
 	private static BeanToDocMapper myInstance;
+	
+	public enum HowMuch {	
+		MINIMUM(0),
+		AVERAGE(4),
+		MAXIMUM(10);
+
+		public int value;
+
+		HowMuch(int value) {
+			this.value = value;
+		}
+		protected int value() {
+			return value;
+		}
+	}
 
 	public static synchronized BeanToDocMapper getInstance() {
 		if (myInstance == null) {
@@ -22,26 +37,49 @@ public class BeanToDocMapper {
 	private BeanToDocMapper() {}
 
 	public IngridDocument mapT01Object(T01Object o) {
+		return mapT01Object(o, HowMuch.MAXIMUM);
+	}
+	public IngridDocument mapT01Object(T01Object o, HowMuch howMuch) {
 		IngridDocument doc = new IngridDocument();
 
-		doc.put(MdekKeys.ENTITY_UUID, o.getId());
-		doc.put(MdekKeys.ENTITY_NAME, o.getObjName());
-//		doc.put(MdekKeys.ENTITY_DESCRIPTION, o.getObjDescr());
+		doc.put(MdekKeys.UUID, o.getId());
+
+		if (howMuch.value() >= HowMuch.AVERAGE.value()) {
+			doc.put(MdekKeys.CLASS, o.getObjClass());
+			doc.put(MdekKeys.TITLE, o.getObjName());			
+		}
+		if (howMuch.value() >= HowMuch.MAXIMUM.value()) {
+			doc.put(MdekKeys.ABSTRACT, o.getObjDescr());
+		}
 		
 		return doc;
 	}
 
 	public IngridDocument mapT02Address(T02Address a) {
+		return mapT02Address(a, HowMuch.MAXIMUM);
+	}
+	public IngridDocument mapT02Address(T02Address a, HowMuch howMuch) {
 		IngridDocument doc = new IngridDocument();
 
-		doc.put(MdekKeys.ENTITY_UUID, a.getId());
+		doc.put(MdekKeys.UUID, a.getId());
 		
-		String name = a.getInstitution();
-		if (name == null) {
-			name = a.getFirstname() + " " + a.getLastname();
+		if (howMuch.value() >= HowMuch.AVERAGE.value()) {
+			doc.put(MdekKeys.CLASS, a.getTyp());
+			doc.put(MdekKeys.ORGANISATION, a.getInstitution());
+			doc.put(MdekKeys.NAME, a.getLastname());
+			doc.put(MdekKeys.GIVEN_NAME, a.getLastname());
+			doc.put(MdekKeys.TITLE_OR_FUNCTION, a.getTitle());
+			doc.put(MdekKeys.STREET, a.getStreet());
+			doc.put(MdekKeys.POSTAL_CODE_OF_COUNTRY, a.getStateId());
+			doc.put(MdekKeys.CITY, a.getCity());
+			doc.put(MdekKeys.POST_BOX_POSTAL_CODE, a.getPostboxPc());
+			doc.put(MdekKeys.POST_BOX, a.getPostbox());
+			doc.put(MdekKeys.FUNCTION, a.getJob());
 		}
-		doc.put(MdekKeys.ENTITY_NAME, name);
-		doc.put(MdekKeys.ENTITY_DESCRIPTION, a.getDescr());
+		if (howMuch.value() >= HowMuch.MAXIMUM.value()) {
+			doc.put(MdekKeys.NAME_FORM, a.getAddress());
+			doc.put(MdekKeys.ADDRESS_DESCRIPTION, a.getDescr());			
+		}
 		
 		return doc;
 	}
