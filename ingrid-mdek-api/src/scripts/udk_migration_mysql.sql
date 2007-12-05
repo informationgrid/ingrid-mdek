b9fab31b26f07f266206c6697ae1432e4498e6a9
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2007-12-05 09:27:56 +0000

    Einfuehrung temporaere Migrationsstand Tabelle in udk DB

--- ingrid-mdek-api/src/scripts/udk_migration_mysql.sql
+++ ingrid-mdek-api/src/scripts/udk_migration_mysql.sql
@@ -1,4 +1,14 @@
 
+-- ==========================================================
+-- TEMPORAERE TABELLE UM DEN MIGRATIONSSTAND EINER DB ZU TRACKEN
+-- NACH JEDER ERGAENZUNG IN DIESEM FILE SOLLTE DIE VERSION HOCHGESETZT WERDEN
+CREATE TABLE `tmp_udk_migrationsstand` (
+  `version` int NOT NULL default '0'
+) TYPE=InnoDB;
+INSERT INTO `tmp_udk_migrationsstand` VALUES (0);
+
+-- ==========================================================
+
 -- VERSION SPALTE HINZUFUEGEN:
 -- -------------------------
 ALTER TABLE t01_object ADD COLUMN version int;
@@ -12,4 +22,7 @@ UPDATE t02_address SET version = 0;
 ALTER TABLE t012_obj_obj CHANGE typ type int NOT NULL default '0';
 ALTER TABLE t012_obj_adr CHANGE typ type int NOT NULL default '0';
 
+-- ==========================================================
+UPDATE tmp_udk_migrationsstand SET version = 1;
+-- ==========================================================
 
