5b9d3e5d898f528286f1420b2e5ddf747dd2f552
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2007-12-13 19:58:08 +0000

    mdek: integrated communication data T021Communication

--- ingrid-mdek-api/src/main/java/de/ingrid/mdek/MdekKeys.java
+++ ingrid-mdek-api/src/main/java/de/ingrid/mdek/MdekKeys.java
@@ -8,57 +8,68 @@ package de.ingrid.mdek;
 public class MdekKeys {
 
     /** List of Object Entities<br>
-     *	<b>Datatype: List of IngridDocs</b> */
+     *	Value: List of IngridDocs */
     public final static String OBJ_ENTITIES = "objEntities";
 
     /** List of Address Entities<br>
-     *	<b>Datatype: List of IngridDocs</b> */
+     *	Value: List of IngridDocs */
     public final static String ADR_ENTITIES = "adrEntities";
 
     /** the uuid of the entity (object or address)<br>
-     *	<b>Datatype: String</b> */
+     *	Value: String */
     public final static String UUID = "uuid";
 
     /** the class of the entity (object or address)<br>
-     *	<b>Datatype: Integer</b> */
+     *	Value: Integer */
     public final static String CLASS = "class";
 
-    /** <b>Datatype: Boolean</b> */
+    /** Value: Boolean */
     public final static String HAS_CHILD = "hasChild";
 
     // OBJECT DATA
     // -----------
 
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String TITLE = "title";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String ABSTRACT = "abstract";
 
     // ADDRESS DATA
     // ------------
 
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String ORGANISATION = "organisation";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String NAME = "name";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String GIVEN_NAME = "given-name";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String NAME_FORM = "name-form";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String TITLE_OR_FUNCTION = "title-or-function";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String STREET = "street";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String POSTAL_CODE_OF_COUNTRY = "postal-code-of-country";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String CITY = "city";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String POST_BOX_POSTAL_CODE = "post-box-postal-code";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String POST_BOX = "post-box";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String FUNCTION = "function";
-    /** <b>Datatype: String</b> */
+    /** Value: String */
     public final static String ADDRESS_DESCRIPTION = "address-description";
+
+    /** Value: List of IngridDocs */
+    public final static String COMMUNICATION = "communication";
+    /** Value: String */
+    public final static String COMMUNICATION_MEDIUM = "communication-medium";
+    /** Value: String */
+    public final static String COMMUNICATION_VALUE = "communication-value";
+    /** Value: String */
+    public final static String COMMUNICATION_DESCRIPTION = "communication-description";
+
+
 }
