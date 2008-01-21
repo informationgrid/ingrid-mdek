d5993e3d928b3d6525cab7caf45feddd563186fa
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-01-21 15:06:55 +0000

    mdek: - handle now ConstraintViolationException - small fixes in MdekExample Client when multi threaded

--- ingrid-mdek-job/src/main/java/de/ingrid/mdek/MdekErrorHandler.java
+++ ingrid-mdek-job/src/main/java/de/ingrid/mdek/MdekErrorHandler.java
@@ -2,6 +2,7 @@ package de.ingrid.mdek;
 
 import org.apache.log4j.Logger;
 import org.hibernate.StaleStateException;
+import org.hibernate.exception.ConstraintViolationException;
 
 import de.ingrid.mdek.MdekErrors.MdekError;
 
@@ -31,7 +32,13 @@ public class MdekErrorHandler {
 
 		if (excIn instanceof MdekException) {
 			// do nothing, we already have an identified error !
+			
 		} else if (excIn instanceof StaleStateException) {
+			// database version is different, someone else changed entity
+			retExc = new MdekException(MdekError.ENTITY_CHANGED_IN_BETWEEN);
+			
+		} else if (excIn instanceof ConstraintViolationException) {
+			// Unique constraint violated, someone else added/updated entity
 			retExc = new MdekException(MdekError.ENTITY_CHANGED_IN_BETWEEN);
 		}
 
