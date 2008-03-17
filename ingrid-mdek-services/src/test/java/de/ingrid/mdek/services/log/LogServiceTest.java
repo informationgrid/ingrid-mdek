d04653d3362286cf3f1dbbcf1c2b40ecfe085aef
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-03-17 17:11:17 +0000

    o use junit4 features

--- ingrid-mdek-services/src/test/java/de/ingrid/mdek/services/log/LogServiceTest.java
+++ ingrid-mdek-services/src/test/java/de/ingrid/mdek/services/log/LogServiceTest.java
@@ -7,11 +7,14 @@ import java.io.FileReader;
 import junit.framework.TestCase;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
 
 import de.ingrid.mdek.job.IJob;
 
-public class LogServiceTest extends TestCase {
+public class LogServiceTest {
 
+    @Test
 	public void testGetLogger() throws Exception {
 		String property = System.getProperty("java.io.tmpdir");
 		File file = new File(property, LogServiceTest.class.getName() + "-"
@@ -23,12 +26,12 @@ public class LogServiceTest extends TestCase {
 		logger.info("test");
 
 		File[] files = file.listFiles();
-		assertEquals(1, files.length);
+		Assert.assertEquals(1, files.length);
 		FileReader reader = new FileReader(files[0]);
 		BufferedReader reader2 = new BufferedReader(reader);
 		String line = reader2.readLine();
-		assertTrue(line.endsWith("test"));
-		assertNull(reader2.readLine());
+		Assert.assertTrue(line.endsWith("test"));
+		Assert.assertNull(reader2.readLine());
 		reader2.close();
 	}
 }
