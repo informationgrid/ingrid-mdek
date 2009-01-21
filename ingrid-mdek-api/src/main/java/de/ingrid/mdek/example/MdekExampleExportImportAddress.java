5ebdd846637be8943d78b709c334666b7bf75a41
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-01-21 10:06:47 +0000

    mdek export: - added storing of exported address data to example

--- ingrid-mdek-api/src/main/java/de/ingrid/mdek/example/MdekExampleExportImportAddress.java
+++ ingrid-mdek-api/src/main/java/de/ingrid/mdek/example/MdekExampleExportImportAddress.java
@@ -1,6 +1,7 @@
 package de.ingrid.mdek.example;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -144,13 +145,12 @@ class MdekExampleExportImportAddressThread extends Thread {
 		// NI catalog
 
 		// OBJECTS
-		String topObjUuid = "3866463B-B449-11D2-9A86-080000507261";
-		// underneath upper top node
 		// 3866463B-B449-11D2-9A86-080000507261
 		//  38664688-B449-11D2-9A86-080000507261
 		//   15C69C20-FE15-11D2-AF34-0060084A4596
 		//    2C997C68-2247-11D3-AF51-0060084A4596
 		//     C1AA9CA6-772D-11D3-AF92-0060084A4596 // leaf
+		String topObjUuid = "3866463B-B449-11D2-9A86-080000507261";
 		String objParentUuid = "15C69C20-FE15-11D2-AF34-0060084A4596";
 		String objUuid = "2C997C68-2247-11D3-AF51-0060084A4596";
 		String objLeafUuid = "C1AA9CA6-772D-11D3-AF92-0060084A4596";
@@ -164,15 +164,24 @@ class MdekExampleExportImportAddressThread extends Thread {
 		String objWithAdditionalFieldsUuid = "3892B136-D1F3-4E45-9E5F-E1CEF117AA74";
 		
 		// ADDRESSES
-		// TOP ADDRESS
+		
+		// NORMAL ONES
+		// 3761E246-69E7-11D3-BB32-1C7607C10000
+		//  C5FEA801-6AB2-11D3-BB32-1C7607C10000
+		//   012CBA17-87F6-11D4-89C7-C1AAE1E96727 // PERSON
 		String topAddrUuid = "3761E246-69E7-11D3-BB32-1C7607C10000";
-		// PARENT ADDRESS (sub address of topUuid)
 		String parentAddrUuid = "C5FEA801-6AB2-11D3-BB32-1C7607C10000";
-		// PERSON ADDRESS (sub address of parentUuid)
 		String personAddrUuid = "012CBA17-87F6-11D4-89C7-C1AAE1E96727";
 		// further non free top addresses (110 top nodes at all)
-		String topAddrUuid2 = "386644BF-B449-11D2-9A86-080000507261";
+//		String topAddrUuid2 = "386644BF-B449-11D2-9A86-080000507261";
 //		String topAddrUuid3 = "4E9DD4F5-BC14-11D2-A63A-444553540000";
+
+		// FREE ONES
+		// 0C3E030A-5A66-4655-87F7-80E24998AA4C
+		// 3F783516-40D7-11D3-AF70-0060084A4596
+		// C40DE0BA-4342-11D3-AF71-0060084A4596
+		// 4E9AC596-6F52-11D3-AF8A-0060084A4596
+		String freeAddrUuid = "0C3E030A-5A66-4655-87F7-80E24998AA4C";
 
 		System.out.println("\n\n----- !!! SWITCH \"CALLING USER\" TO CATALOG ADMIN (all permissions) -----");
 		doc = supertool.getCatalogAdmin();
@@ -213,13 +222,43 @@ class MdekExampleExportImportAddressThread extends Thread {
 		System.out.println("\n----- get LAST Export Info -----");
 		supertool.getExportInfo(false);
 
+		System.out.println("\n----- export addresses TOP ADDRESS -----");
+		supertool.exportAddressBranch(topAddrUuid, true, null);
+		supertool.setFullOutput(true);
+		result = supertool.getExportInfo(true);
+		supertool.setFullOutput(false);
+		String exportTopAddrUnzipped = "";
+		try {
+			exportTopAddrUnzipped = MdekUtils.decompressZippedByteArray((byte[]) result.get(MdekKeys.EXPORT_RESULT));
+		} catch(IOException ex) {
+			System.out.println(ex);
+		}
+		
+		System.out.println("\n----- export addresses FREE ADDRESS -----");
+		supertool.exportAddressBranch(freeAddrUuid, true, null);
+		supertool.setFullOutput(true);
+		result = supertool.getExportInfo(true);
+		supertool.setFullOutput(false);
+		String exportFreeAddrUnzipped = "";
+		try {
+			exportFreeAddrUnzipped = MdekUtils.decompressZippedByteArray((byte[]) result.get(MdekKeys.EXPORT_RESULT));
+		} catch(IOException ex) {
+			System.out.println(ex);
+		}
+
 		System.out.println("\n----- export addresses ONLY PARENT NODE -----");
 		supertool.exportAddressBranch(parentAddrUuid, true, null);
 		supertool.getExportInfo(true);
 
 		System.out.println("\n----- export addresses FULL BRANCH UNDER PARENT -----");
 		supertool.exportAddressBranch(parentAddrUuid, false, null);
-		supertool.getExportInfo(true);
+		result = supertool.getExportInfo(true);
+		String exportAddrBranchUnzipped = "";
+		try {
+			exportAddrBranchUnzipped = MdekUtils.decompressZippedByteArray((byte[]) result.get(MdekKeys.EXPORT_RESULT));
+		} catch(IOException ex) {
+			System.out.println(ex);
+		}
 
 		System.out.println("\n----- export addresses ALL TOP NON FREE ADDRESSES -----");
 		supertool.exportAddressBranch(null, true, AddressArea.ALL_NON_FREE_ADDRESSES);
