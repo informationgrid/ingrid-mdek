44b0253db1237000a63eff09f879963fcf96b6f8
Toan Tran <khanh.toan.tran@wemove.com>
2015-03-26 15:07:57 +0100

    Remove JAVA_HEAP_MAX and add "-XX:+UseG1GC -XX:+UseStringDeduplication -XX:NewRatio=3" to INGRID_OPTS.

--- ingrid-mdek-job/src/main/release/scripts/start.sh
+++ ingrid-mdek-job/src/main/release/scripts/start.sh
@@ -171,13 +171,6 @@ startIplug()
   fi
   
   JAVA=$JAVA_HOME/bin/java
-  JAVA_HEAP_MAX="-Xmx256m"
-  
-  # check envvars which might override default args
-  if [ "$INGRID_HEAPSIZE" != "" ]; then
-    JAVA_HEAP_MAX="-Xmx""$INGRID_HEAPSIZE""m"
-    echo "run with heapsize $JAVA_HEAP_MAX"
-  fi
 
   # CLASSPATH initially contains $INGRID_CONF_DIR, or defaults to $INGRID_HOME/conf
   CLASSPATH=${CLASSPATH}:${INGRID_CONF_DIR:=$INGRID_HOME/conf}
@@ -199,11 +192,11 @@ startIplug()
   fi
 
   export CLASSPATH="$CLASSPATH"
-  INGRID_OPTS="$INGRID_OPTS -Dingrid_home=$INGRID_HOME"
+  INGRID_OPTS="$INGRID_OPTS -Dingrid_home=$INGRID_HOME -XX:+UseG1GC -XX:+UseStringDeduplication -XX:NewRatio=3"
   CLASS=de.ingrid.mdek.MdekServer
   
   # run it
-  exec nohup "$JAVA" $JAVA_HEAP_MAX $INGRID_OPTS $CLASS --descriptor conf/communication.xml > logs/console.log &
+  exec nohup "$JAVA" $INGRID_OPTS $CLASS --descriptor conf/communication.xml > logs/console.log &
   
   echo "ingrid component ($INGRID_HOME) started."
   echo $! > $PID
