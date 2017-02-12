9d2eefc795b17e285f64d112d9f98b2244522f1f
jm <joachim@wemove.com>
2017-02-12 21:51:37 +0100

    Add BAW DMQS specific mapping to add the center of "Bwstr Abschnitte" using the Bwstr locator at
    https://atlas.wsv.bund.de/bwastr-locator/rest/geokodierung/query
    
    The center of a "Bwstr Abschnitt" defined by BwstrId, kmFrom, kmTo is
    indexed to the index fields bwstr-center-lat and bwstr-center-lon.
    
    The functionality is used to visualize the BAW DMQS metadata on a map.

---
+++ ingrid-mdek-job/src/main/release/presets/baw-dmqs/mapping/baw-dmqs-postprocessing.js
@@ -0,0 +1,59 @@
+/*
+ * **************************************************-
+ * InGrid-iPlug DSC
+ * ==================================================
+ * Copyright (C) 2014 - 2016 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * **************************************************#
+ */
+if (javaVersion.indexOf( "1.8" ) === 0) {
+	load("nashorn:mozilla_compat.js");
+}
+
+importPackage(Packages.org.apache.lucene.document);
+importPackage(Packages.de.ingrid.iplug.dsc.om);
+importPackage(Packages.de.ingrid.geo.utils.transformation);
+
+var BwstrLocUtil = Java.type("de.ingrid.iplug.dsc.utils.BwstrLocUtil");
+var BWST_LOC_TOOL = new BwstrLocUtil();
+
+
+if (log.isDebugEnabled()) {
+	log.debug("Mapping source record to lucene document: " + sourceRecord.toString());
+}
+
+if (!(sourceRecord instanceof DatabaseSourceRecord)) {
+    throw new IllegalArgumentException("Record is no DatabaseRecord!");
+}
+
+var doc = IDX.getDocument();
+var bwstrIdAndKm = doc.get('location');
+
+log.debug("Found bwstrIdAndKm: " + bwstrIdAndKm);
+
+if (BWST_LOC_TOOL.isBwstrIdAndKm(bwstrIdAndKm)) {
+	var center = BWST_LOC_TOOL.getCenterFromBwstrIdAndKm(bwstrIdAndKm);
+	if (center && center.length==2) {
+		IDX.addNumeric("bwstr-center-lon", center[0]);
+		IDX.addNumeric("bwstr-center-lat", center[1]);
+	}
+}
+
+
+
+
+
