d85536c32a2a378f5c1e07fe990c7044f8699257
Andre Wallat <andre.wallat@wemove.com>
2017-04-25 13:26:46 +0200

    - replace new lines with semicolons when importing multi lineage-fields (#204)

--- ingrid-mdek-job/src/main/resources/import/mapper/csw202_to_ingrid_igc.js
+++ ingrid-mdek-job/src/main/resources/import/mapper/csw202_to_ingrid_igc.js
@@ -218,7 +218,7 @@ var mappingDescription = {"mappings":[
 	        		{	
 	        		    "srcXpath":"//gmd:dataQualityInfo/gmd:DQ_DataQuality/gmd:lineage/gmd:LI_Lineage/gmd:source/gmd:LI_Source/gmd:description/gco:CharacterString",
 	        		    "targetNode":"/igc/data-sources/data-source/data-source-instance/technical-domain/map/data-basis-text",
-        		        "concatEntriesWith":"\n"
+        		        "concatEntriesWith":"; "
 	        		},
 	        		{	
 	        			"srcXpath":"//gmd:dataQualityInfo/gmd:DQ_DataQuality/gmd:lineage/gmd:LI_Lineage/gmd:statement/gco:CharacterString",
