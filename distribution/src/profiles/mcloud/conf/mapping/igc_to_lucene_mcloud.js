62db09d9ebd797ada7c7ff1dc14dd5ee6d81ab7c
Andre Wallat <andre.wallat@wemove.com>
2018-06-22 12:25:54 +0200

    Index more fields

--- distribution/src/profiles/mcloud/conf/mapping/igc_to_lucene_mcloud.js
+++ distribution/src/profiles/mcloud/conf/mapping/igc_to_lucene_mcloud.js
@@ -35,59 +35,19 @@ var BOOST_NO_COUPLED_RESOURCE  = 0.9;
 var BOOST_HAS_COUPLED_RESOURCE = 1.0;
 
 if (log.isDebugEnabled()) {
-    log.debug("Mapping source record to lucene document: " + sourceRecord.toString());
+    log.debug('Mapping source record to lucene document: ' + sourceRecord.toString());
 }
 
 if (!(sourceRecord instanceof DatabaseSourceRecord)) {
-    throw new IllegalArgumentException("Record is no DatabaseRecord!");
+    throw new IllegalArgumentException('Record is no DatabaseRecord!');
 }
 
 // add default boost value
 IDX.addDocumentBoost(1.0);
 
-// add UVP specific mapping
-// UVP Codelist
-var behavioursValueRow = SQL.first("SELECT * FROM sys_generic_key WHERE key_name='BEHAVIOURS'");
-var codelist = '';
-var publishNegativeExaminations = false;
-if (hasValue(behavioursValueRow)){
-    var behaviours = behavioursValueRow.get("value_string");
-    if(hasValue(behaviours)){
-        var behavioursJson = JSON.parse(behaviours);
-        for(var beh in behavioursJson){
-            var behaviour = behavioursJson[beh];
-            if(hasValue(behaviour)){
-                var behaviourId = behaviour.id;
-                if(hasValue(behaviourId)){
-                    if (behaviourId.equals("uvpPhaseField")){
-                        var behaviourParams = behaviour.params;
-                        if (hasValue(behaviourParams)){
-                            for(var behParam in behaviourParams){
-                                var behaviourParam = behaviourParams[behParam];
-                                if (hasValue(behaviourParam)){
-                                    var behaviourParamId = behaviourParam.id;
-                                    if (behaviourParamId.equals("categoryCodelist")){
-                                        var behaviourParamValue = behaviourParam.value;
-                                        if (hasValue(behaviourParamValue)){
-                                            codelist = behaviourParamValue;
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    } else if (behaviourId.equals("uvpPublishNegativeExamination")) {
-                        publishNegativeExaminations = behaviour.active;
-                    }
-                }
-            }
-        }
-    }
-}
-
 // ---------- t01_object ----------
-var objId = sourceRecord.get("id");
-var objRows = SQL.all("SELECT * FROM t01_object WHERE id=?", [objId]);
+var objId = sourceRecord.get('id');
+var objRows = SQL.all('SELECT * FROM t01_object WHERE id=?', [objId]);
 for (var i=0; i<objRows.size(); i++) {
 /*
     // Example iterating all columns !
@@ -99,100 +59,147 @@ for (var i=0; i<objRows.size(); i++) {
     }
 */
     var objRow = objRows.get(i);
-    var objClass = objRow.get("obj_class");
+    var objClass = objRow.get('obj_class');
 
     // skip negative examinations if not defined otherwise in catalog settings
-    // if (!publishNegativeExaminations && objClass === "12") {
-    //     throw new SkipException("Catalog settings say not to publish negative examinations");
+    // if (!publishNegativeExaminations && objClass === '12') {
+    //     throw new SkipException('Catalog settings say not to publish negative examinations');
     // }
 
-    IDX.add("id", objRow.get("id"));
-    IDX.add("uuid", objRow.get("obj_uuid"));
+    IDX.add('id', objRow.get('id'));
+    IDX.add('uuid', objRow.get('obj_uuid'));
 
     // title, description, addresses, termsOfUse, category, downloads, license, quellenvermerk, mFund, geothesaurus, Zeitberzug, Zeitspanne, Periodizität
-    IDX.add("title", objRow.get("obj_name"));
-    IDX.add("description", objRow.get("obj_descr"));
-    IDX.add("extras.subgroups", getCategories(objId));
+    IDX.add('title', objRow.get('obj_name'));
+    IDX.add('description', objRow.get('obj_descr'));
+
+    var extras = createExtras(objId, objRow);
+    IDX.addAllFromJSON('{ "extras": { ' + extras + ' } }');
+
     var dists = getDistributions(objId);
-    for (var i=0; i<dists.length; i++) {
-        IDX.add("distribution", JSON.stringify(dists[i]));
-    }
-    IDX.add("extras.license_id", null);
-    IDX.add("extras.license_url", null);
-    IDX.add("organization", [getOrganization()]);
+    IDX.addAllFromJSON('{ "distribution": [' + dists.join(',') + ']}');
+    IDX.addAllFromJSON('{ "publisher": ' + getOrganizations(objId) + '}');
 
 }
 
+function createExtras(objId, objRow) {
+    var extrasArray = [];
+    extrasArray.push( '"subgroups": ' + getCategories(objId) );
+    extrasArray.push( '"license_id": "' + getAdditionalFieldData(objId, 'mcloudLicense') + '"' );
+    extrasArray.push( '"license_url": "' + getAdditionalFieldData(objId, 'mcloudLicenseUrlXXX') + '"' );
+    extrasArray.push( '"terms_of_use": "' + getAdditionalFieldData(objId, 'mcloudTermsOfUse') + '"' );
+    extrasArray.push( '"realtime": ' + (objRow.get('time_period') === '1' ? 'true' : 'false') );
+
+    log.debug("realtime: " + objRow.get('time_period'));
+
+    return extrasArray.join(',');
+}
+
 function getCategories(objId) {
-    var mcloudCategories = getAdditionalField(objId, "mcloudCategory");
+    var mcloudCategories = getAdditionalField(objId, 'mcloudCategory');
     if (mcloudCategories) {
         var categories = [];
-        categories.push(mcloudCategories.get("list_item_id"));
-        log.debug("resultCategory: " + categories);
-        return categories;
+        categories.push(mcloudCategories.get('list_item_id'));
+        log.debug('resultCategory: ' + categories);
+        return JSON.stringify(categories);
     }
     return [];
 }
 
 function getDistributions(objId) {
     var distributions = [];
-    var table = getAdditionalFieldTable(objId, "mcloudDownloads");
+    var table = getAdditionalFieldTable(objId, 'mcloudDownloads');
     if (table) {
         for (var i=0; i<table.length; i++) {
             var row = table[i];
-            log.debug("distribution:" + JSON.stringify(row));
+            log.debug('distribution:' + JSON.stringify(row));
 
-            distributions.push({
-                format: row["dateFormat"],
-                accessUrl: row["link"]
-            });
+            distributions.push(JSON.stringify({
+                format: row['dateFormat'],
+                accessUrl: row['link']
+            }));
         }
     }
     return distributions;
 }
 
-function getOrganization() {
-    return null;
+function getOrganizations(objId) {
+    var publisher = [];
+
+    var rows = SQL.all("SELECT * FROM t012_obj_adr WHERE obj_id=?", [+objId]);
+    for (j=0; j<rows.size(); j++) {
+        var addrUuid = rows.get(j).get("adr_uuid");
+
+        var addrNodeRows = SQL.all("SELECT * FROM address_node WHERE addr_uuid=? AND addr_id_published IS NOT NULL", [addrUuid]);
+        for (k=0; k<addrNodeRows.size(); k++) {
+            var parentAddrUuid = addrNodeRows.get(k).get("fk_addr_uuid");
+            var addrIdPublished = addrNodeRows.get(k).get("addr_id_published");
+
+            var addrRow = SQL.first("SELECT * FROM t02_address WHERE id=? and (hide_address IS NULL OR hide_address != 'Y')", [+addrIdPublished]);
+            if (hasValue(addrRow)) {
+                var homepage = "";
+                var commRows = SQL.all("SELECT * FROM t021_communication WHERE adr_id=?", [+addrIdPublished]);
+                for (l=0; l<commRows.size(); l++) {
+                    // TODO: only add emails! (just one needed!)
+                    if (commRows.get(l).get('commtype_key') === '3') {
+                        homepage = commRows.get(l).get('comm_value');
+                        break;
+                    }
+                }
+
+                publisher.push({
+                    "organization": addrRow.get('institution'),
+                    "homepage": homepage
+                });
+
+            }
+        }
+    }
+    return JSON.stringify(publisher);
 }
 
 function getAdditionalField(objId, additionalFieldId) {
-    var row = SQL.first("SELECT * FROM additional_field_data WHERE obj_id=? AND field_key=?", [objId, additionalFieldId]);
-    if (hasValue(row) && hasValue(row.get("id"))) {
+    var row = SQL.first('SELECT * FROM additional_field_data WHERE obj_id=? AND field_key=?', [objId, additionalFieldId]);
+    if (hasValue(row) && hasValue(row.get('id'))) {
         return row;
     }
     return null;
 }
 
+function getAdditionalFieldData(objId, additionalFieldId) {
+    var field = getAdditionalField(objId, additionalFieldId);
+    return (field === null) ? '' : field.get('data');
+}
 
 function getAdditionalFieldTable(objId, additionalFieldId) {
     var rows = [];
     var table = getAdditionalField(objId, additionalFieldId);
-    var tableResult = SQL.all("SELECT * FROM additional_field_data WHERE parent_field_id=? ORDER BY sort", [table.get("id")]);
+    var tableResult = SQL.all('SELECT * FROM additional_field_data WHERE parent_field_id=? ORDER BY sort', [table.get('id')]);
     // for (var i=0; i<tableResult.size(); i++) {
         var i = 0;
-        var sort = "1";
+        var sort = '1';
         var row = {};
         var processing = true;
         while (processing) {
-            log.debug("processing: " + i);
+            log.debug('processing: ' + i);
             var column = tableResult.get(i);
-            log.debug("processing: " + column);
-            if (column.get("sort") === sort) {
-                row[column.get("field_key")] = column.get("data");
+            log.debug('processing: ' + column);
+            if (column.get('sort') === sort) {
+                row[column.get('field_key')] = column.get('data');
                 i++;
             } else {
-                sort = column.get("sort");
+                sort = column.get('sort');
                 rows.push(row);
                 row = {};
             }
-            log.debug("sort: " + sort);
+            log.debug('sort: ' + sort);
             if (i === tableResult.size()) {
                 rows.push(row);
                 processing = false;
             }
         }
     // }
-    log.debug("result table: " + rows);
-    log.debug("result table: " + rows.length);
+    log.debug('result table: ' + rows);
+    log.debug('result table: ' + rows.length);
     return rows;
 }
