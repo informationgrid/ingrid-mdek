a984ddec7c4bcac9f0b43103c6ee2d3eb27242fc
Andre Wallat <andre.wallat@wemove.com>
2018-09-14 09:50:03 +0200

    Add license source as another otherConstraint to IDF

--- ingrid-mdek-job/src/main/resources/mapping/igc_to_idf.js
+++ ingrid-mdek-job/src/main/resources/mapping/igc_to_idf.js
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.1 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl5
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -1993,6 +1993,9 @@ function addResourceConstraints(identificationInfo, objRow) {
                     var licenseJSONParsed = JSON.parse(licenseJSON);
                     licenseJSONParsed.quelle = licenseSource;
                     licenseJSON = JSON.stringify(licenseJSONParsed);
+
+                    // add license source also as additional otherConstraint (#1066)
+                    mdLegalConstraints.addElement("gmd:otherConstraints/gco:CharacterString").addText("Quellenvermerk: " + licenseSource);
                 }
                 mdLegalConstraints.addElement("gmd:otherConstraints/gco:CharacterString").addText(licenseJSON);
             }
