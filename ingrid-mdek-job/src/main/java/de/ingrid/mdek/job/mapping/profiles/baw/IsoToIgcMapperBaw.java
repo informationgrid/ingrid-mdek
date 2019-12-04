111abbe2edc14f433264cea0760d6e1d8508a319
Vikram Notay <vikram.notay@baw.de>
2019-12-04 09:42:54 +0100

    BAW: Import BWaStr. identifiers from ISO (#1417)

--- ingrid-mdek-job/src/main/java/de/ingrid/mdek/job/mapping/profiles/baw/IsoToIgcMapperBaw.java
+++ ingrid-mdek-job/src/main/java/de/ingrid/mdek/job/mapping/profiles/baw/IsoToIgcMapperBaw.java
@@ -24,14 +24,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IsoToIgcMapperBaw implements ImportDataMapper<Document, Document> {
 import static de.ingrid.mdek.job.mapping.profiles.baw.BawConstants.BAW_HIERARCHY_LEVEL_NAME_CODELIST_ID;
 
     private static final Logger LOG = Logger.getLogger(IsoToIgcMapperBaw.class);
+
+    /**
+     * [^0-9]*: any character, as long as it isn't a number
+     * ([0-9]{4}): First capture group with exactly 4 digits
+     * (-[0-9.]+)?: Second capture group starting with a hyphen and followed by a number (int or float). The whole capture group is optional (Question mark at the end)
+     * ditto
+     * .*: any trailing character
+     */
+    private static final Pattern BWASTR_PATTERN = Pattern.compile("[^0-9]*([0-9]{4})(-[0-9.]+)?(-[0-9.]+)?.*");
 
     private MdekCatalogService catalogService;
 
@@ -75,6 +87,7 @@ import static de.ingrid.mdek.job.mapping.profiles.baw.BawConstants.BAW_HIERARCHY
 
             mapAuftragInfos(mdIdentification, additionalValues);
             mapHierarchyLevelName(mdMetadata, additionalValues, protocolHandler);
+            mapBWaStrIdentifiers(mdIdentification, additionalValues, protocolHandler);
 
         } catch (MdekException e) {
             protocolHandler.addMessage(Type.ERROR, e.getMessage());
@@ -134,6 +147,81 @@ import static de.ingrid.mdek.job.mapping.profiles.baw.BawConstants.BAW_HIERARCHY
             additionalValue.addElement("field-data").
                     addText(pspNumber);
         }
+    }
+
+    private void mapBWaStrIdentifiers(Element mdIdentification, Element additionalValues, ProtocolHandler ph) {
+        String codeXpath = "./gmd:extent//gmd:geographicIdentifier/gmd:MD_Identifier[./gmd:authority/gmd:CI_Citation/gmd:title/gco:CharacterString/text()='VV-WSV 1103']/gmd:code/gco:CharacterString";
+        NodeList codeNodes = isoXpathUtil.getNodeList(mdIdentification, codeXpath);
+        for(int i=0; i<codeNodes.getLength(); i++) {
+            String code = getNodeText(codeNodes.item(i));
+            if (!code.isEmpty()) {
+                LOG.debug("Found BWaStr. Section identifier: " + code);
+
+                // Add the code as additional value
+                String idx = Integer.toString(i+1); // Line numbers start with 1
+
+                String[] components = toBWaStrComponents(code);
+                String bwastrId = components[0];
+                String kmStart = components[1];
+                String kmEnd = components[2];
+
+                if (bwastrId == null) {
+                    ph.addMessage(Type.WARN, "No BWaStr. ID detected for code: " + code);
+                    continue;
+                }
+
+                IdfElement idAddnValue = igcDomUtil.addElement(additionalValues, "general-additional-value")
+                        .addAttribute("line", idx);
+                idAddnValue.addElement("field-key")
+                        .addText("bwastr_name");
+                idAddnValue.addElement("field-data")
+                        .addAttribute("id", "-1")
+                        .addText(Integer.toString(Integer.parseInt(bwastrId))); // Get rid of leading zeros
+                idAddnValue.addElement("field-key-parent")
+                        .addText("bwastrTable");
+
+                if (kmStart != null) {
+                    IdfElement kmStartAddnValue = igcDomUtil.addElement(additionalValues, "general-additional-value")
+                            .addAttribute("line", idx);
+                    kmStartAddnValue.addElement("field-key")
+                            .addText("bwastr_km_start");
+                    kmStartAddnValue.addElement("field-data")
+                            .addAttribute("id", "-1")
+                            .addText(kmStart);
+                    kmStartAddnValue.addElement("field-key-parent")
+                            .addText("bwastrTable");
+                }
+
+                if (kmEnd != null) {
+                    IdfElement kmStartAddnValue = igcDomUtil.addElement(additionalValues, "general-additional-value")
+                            .addAttribute("line", idx);
+                    kmStartAddnValue.addElement("field-key")
+                            .addText("bwastr_km_end");
+                    kmStartAddnValue.addElement("field-data")
+                            .addAttribute("id", "-1")
+                            .addText(kmEnd);
+                    kmStartAddnValue.addElement("field-key-parent")
+                            .addText("bwastrTable");
+                }
+
+
+                // Delete from uncontrolled geolocations
+                String locXpath = "../../spatial-domain/geo-location[./uncontrolled-location/location-name/text()='" + code + "']";
+                igcXpathUtil.removeElementAtXPath(additionalValues, locXpath);
+            }
+
+        }
+    }
+
+    private String[] toBWaStrComponents(String code) {
+        Matcher matcher = BWASTR_PATTERN.matcher(code);
+        matcher.find(); // Only consider the first match
+
+        String id = matcher.group(1);
+        String start = matcher.group(2) == null ? null : matcher.group(2).substring(1); // Trim the hyphen at the start
+        String end = matcher.group(3) == null ? null : matcher.group(3).substring(1);
+
+        return new String[] { id, start, end };
     }
 
     private String getNodeText(Node node) {
