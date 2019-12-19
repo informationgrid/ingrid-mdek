2c3067a4827c94424d7a1efee1523d37ea81594b
Joachim Müller <joachim.mueller@wemove.com>
2019-12-19 17:56:51 +0100

    Revert commit for #1044
    This commit should be included on request in a bug fix release.
    
    Original commit message:
    
    Update gml name space uri (#1044)
    
    The gml namespace uri has been changed in the metadata specification.
    The GDI-DE uses the new GML-Version 3.2 namespace for all datasets
    since Dez. 2019.

--- ingrid-mdek-job/src/main/resources/mapping/igc_to_idf_address.js
+++ ingrid-mdek-job/src/main/resources/mapping/igc_to_idf_address.js
@@ -40,7 +40,7 @@ if (!(sourceRecord instanceof DatabaseSourceRecord)) {
 DOM.addNS("gmd", "http://www.isotc211.org/2005/gmd");
 DOM.addNS("gco", "http://www.isotc211.org/2005/gco");
 DOM.addNS("srv", "http://www.isotc211.org/2005/srv");
-DOM.addNS("gml", "http://www.opengis.net/gml/3.2");
+DOM.addNS("gml", "http://www.opengis.net/gml");
 DOM.addNS("gts", "http://www.isotc211.org/2005/gts");
 DOM.addNS("xlink", "http://www.w3.org/1999/xlink");
 
