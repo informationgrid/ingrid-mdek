e78daee7f303937241b7da79852d743076c59bff
Benjamin Röhrl <b.roehrl@gmail.com>
2020-07-01 18:23:46 +0200

    Fix wrong script code

--- distribution/src/docker/files/wait-for-it.sh
+++ distribution/src/docker/files/wait-for-it.sh
@@ -1,26 +1,79 @@
-FROM alpine AS builder
+#!/bin/sh
 
-COPY ingrid-iplug-*-installer.jar ./files /
-RUN unzip ingrid-iplug-*-installer.jar -d /tmp \
-    && export INSTALL_DIR=`ls -d1 /tmp/ingrid-iplug-*` \
-    && mkdir -p $INSTALL_DIR/logs \
-    && touch $INSTALL_DIR/pattern \
-    && cp run.sh $INSTALL_DIR/ \
-    && cp wait-for-it.sh $INSTALL_DIR/ \
-    && cp waitFor $INSTALL_DIR/ \
-    && sed -i -r 's/(<AppenderRef.*\/>)/\1<AppenderRef ref="Console" \/>/g' $INSTALL_DIR/conf/log4j2.xml \
-    && echo jetty.port=8080 > $INSTALL_DIR/conf/config.override.properties
+TIMEOUT=15
+QUIET=0
 
-FROM docker-registry.wemove.com/ingrid-java:jre-8-alpine
-ENV INGRID_USER=ingrid
-ENV RUN_DIRECTLY=true
+echoerr() {
+  if [ "$QUIET" -ne 1 ]; then printf "%s\n" "$*" 1>&2; fi
+}
 
-# user, group ingrid (1000:1000) is created in base image
-COPY --chown=1000:1000 --from=builder /tmp/ingrid-iplug-ige* /opt/ingrid/ingrid-iplug-ige/
+usage() {
+  exitcode="$1"
+  cat << USAGE >&2
+Usage:
+  $cmdname host:port [-t timeout] [-- command args]
+  -q | --quiet                        Do not output any status messages
+  -t TIMEOUT | --timeout=timeout      Timeout in seconds, zero for no timeout
+  -- COMMAND ARGS                     Execute command with args after the test finishes
+USAGE
+  exit "$exitcode"
+}
 
-USER ingrid
+wait_for() {
+  for i in `seq $TIMEOUT` ; do
+    nc -z "$HOST" "$PORT" > /dev/null 2>&1
 
-WORKDIR /opt/ingrid/ingrid-iplug-ige
-EXPOSE 8080
+    result=$?
+    if [ $result -eq 0 ] ; then
+      if [ $# -gt 0 ] ; then
+        exec "$@"
+      fi
+      exit 0
+    fi
+    sleep 1
+  done
+  echo "Operation timed out" >&2
+  exit 1
+}
 
-CMD /bin/sh start.sh start && tail -f /dev/null
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    *:* )
+    HOST=$(printf "%s\n" "$1"| cut -d : -f 1)
+    PORT=$(printf "%s\n" "$1"| cut -d : -f 2)
+    shift 1
+    ;;
+    -q | --quiet)
+    QUIET=1
+    shift 1
+    ;;
+    -t)
+    TIMEOUT="$2"
+    if [ "$TIMEOUT" = "" ]; then break; fi
+    shift 2
+    ;;
+    --timeout=*)
+    TIMEOUT="${1#*=}"
+    shift 1
+    ;;
+    --)
+    shift
+    break
+    ;;
+    --help)
+    usage 0
+    ;;
+    *)
+    echoerr "Unknown argument: $1"
+    usage 1
+    ;;
+  esac
+done
+
+if [ "$HOST" = "" -o "$PORT" = "" ]; then
+  echoerr "Error: you need to provide a host and port to test."
+  usage 2
+fi
+
+wait_for "$@"
