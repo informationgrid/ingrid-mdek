851c8e15ebf379fca60415f39f51ab9d74f62233
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2021-01-14 12:29:39 +0100

    Fix iso export of 'Relative Pisitional Accuracy #1783

--- ingrid-mdek-job/src/main/resources/mapping/igc_to_idf_obj_dq.js
+++ ingrid-mdek-job/src/main/resources/mapping/igc_to_idf_obj_dq.js
@@ -677,10 +677,7 @@ function addObjectDataQualityTable(objRow, dqDataQuality) {
             }
             var dqElem = dqDataQuality.addElement("gmd:report/gmd:DQ_RelativeInternalPositionalAccuracy");
             dqElem.addElement("gmd:nameOfMeasure/gco:CharacterString").addText(igcNameOfMeasureValue);
-            // if (igcNameOfMeasureKey.equals("1")) {
-            //     // Attribute value uncertainty at 95 % significance level
-            //     dqElem.addElement("gmd:measureIdentification/gmd:MD_Identifier/gmd:code/gco:CharacterString").addText("71");
-            // }
+            dqElem.addElement("gmd:measureIdentification/gmd:MD_Identifier/gmd:code/gco:CharacterString").addText("28")
             if (hasValue(igcMeasureDescription)) {
                 dqElem.addElement("gmd:measureDescription/gco:CharacterString").addText(igcMeasureDescription);
             }
@@ -688,9 +685,9 @@ function addObjectDataQualityTable(objRow, dqDataQuality) {
             var unitDefinition = dqQuantitativeResult.addElement("gmd:valueUnit/gml:UnitDefinition")
                 .addAttribute("gml:id", "unitDefinition_ID_".concat(TRANSF.getRandomUUID()));
             unitDefinition.addElement("gml:identifier").addAttribute("codeSpace", "");
-            unitDefinition.addElement("gml:name").addText("percent");
-            unitDefinition.addElement("gml:quantityType").addText("relative external positional accuracy");
-            unitDefinition.addElement("gml:catalogSymbol").addText("%");
+            unitDefinition.addElement("gml:name").addText("meter");
+            unitDefinition.addElement("gml:quantityType").addText("relative internal positional accuracy");
+            unitDefinition.addElement("gml:catalogSymbol").addText("m");
 
             dqQuantitativeResult.addElement("gmd:value/gco:Record").addText(igcResultValue);
         }
