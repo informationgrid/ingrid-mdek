f644a8500caf32d0f2ae0d309ebabf1e9320ba94
Vikram Notay <vikram.notay@baw.de>
2021-04-07 09:07:38 +0200

    Remove duplicate GetCapabilities links (#2171)
    Restore functionality that removes duplicate GetCapabilities links from
    the linkage section.

---
+++ distribution/src/profiles/baw_mis/conf/mapping/igc_to_idf_nokis.js
@@ -0,0 +1,61 @@
+/*
+ * **************************************************-
+ * InGrid-iPlug DSC
+ * ==================================================
+ * Copyright (C) 2014 - 2021 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * **************************************************#
+ */
+if (javaVersion.indexOf( "1.8" ) === 0) {
+	load("nashorn:mozilla_compat.js");
+	CAPABILITIES = Java.type('de.ingrid.utils.capabilities.CapabilitiesUtils');
+}
+
+importPackage(Packages.org.w3c.dom);
+importPackage(Packages.de.ingrid.iplug.dsc.om);
+
+if (log.isDebugEnabled()) {
+    log.debug("Nokis: Mapping source record to idf document: " + sourceRecord.toString());
+}
+
+if (!(sourceRecord instanceof DatabaseSourceRecord)) {
+    throw new IllegalArgumentException("Record is no DatabaseRecord!");
+}
+
+var objId = sourceRecord.get("id");
+
+// objClass variable is from script before "igc_to_idf.js"
+if (objClass.equals("1")) {
+    removeCoupledServiceInformation();
+}
+
+function removeCoupledServiceInformation() {
+    var linkages = XPATH.getNodeList(idfDoc, "//gmd:linkage/gmd:URL");
+    for (var k = 0; k < linkages.length; k++) {
+        var linkage = linkages.item(k);
+
+        removeServiceGetCapabilitiesURL(linkage);
+    }
+}
+
+function removeServiceGetCapabilitiesURL(linkage) {
+    var name = XPATH.getString(linkage, "../../gmd:name/gco:CharacterString");
+    if (hasValue(name) && name.indexOf("Dienst \"") === 0 && name.indexOf("(GetCapabilities)") !== -1) {
+        XPATH.removeElementAtXPath(linkage, "../../../../..");
+    }
+}
+
