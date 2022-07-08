9ab3ef8a0d9cf97fa727729506a26a0e304105f3
André Wallat <andre.wallat@wemove.com>
2022-07-08 09:35:55 +0200

    Fix setting parent identifier for root objects

--- ingrid-mdek-job/src/main/resources/mapping/igc_to_idf.js
+++ ingrid-mdek-job/src/main/resources/mapping/igc_to_idf.js
@@ -135,17 +135,19 @@ for (i=0; i<objRows.size(); i++) {
         mdMetadata.addElement("gmd:parentIdentifier/gco:CharacterString").addText(explicitParentIdentifier);
     } else {
         rows = SQL.all("SELECT objNode1.fk_obj_uuid, obj.org_obj_id FROM object_node objNode1, object_node objNode2, t01_object obj WHERE objNode1.fk_obj_uuid=objNode2.obj_uuid AND objNode2.obj_id=obj.id AND objNode1.obj_uuid=?", [objUuid]);
-        // Should be only one row !
-        objParentUuid = rows.get(0).get("org_obj_id");
-        if (!hasValue(objParentUuid)) {
-            objParentUuid = rows.get(0).get("fk_obj_uuid");
-        }
-        if (hasValue(objParentUuid)) {
-            // check if parent is a folder
-            // this query normally shoud return no value if parent is a folder, since they are never published ("V")
-            var parentObjRow = SQL.first("SELECT obj_class FROM t01_object WHERE (org_obj_id=? OR obj_uuid=?) and work_state=?", [objParentUuid, objParentUuid, "V"]);
-            if (hasValue(parentObjRow) && !parentObjRow.get("obj_class").equals("1000")) {
-                mdMetadata.addElement("gmd:parentIdentifier/gco:CharacterString").addText(objParentUuid);
+        // Should be max one row!
+        if (rows.size() > 0) {
+            objParentUuid = rows.get(0).get("org_obj_id");
+            if (!hasValue(objParentUuid)) {
+                objParentUuid = rows.get(0).get("fk_obj_uuid");
+            }
+            if (hasValue(objParentUuid)) {
+                // check if parent is a folder
+                // this query normally shoud return no value if parent is a folder, since they are never published ("V")
+                var parentObjRow = SQL.first("SELECT obj_class FROM t01_object WHERE (org_obj_id=? OR obj_uuid=?) and work_state=?", [objParentUuid, objParentUuid, "V"]);
+                if (hasValue(parentObjRow) && !parentObjRow.get("obj_class").equals("1000")) {
+                    mdMetadata.addElement("gmd:parentIdentifier/gco:CharacterString").addText(objParentUuid);
+                }
             }
         }
     }
