ab48cd137a330a84f239a390b28e9526876177b8
Toan Tran <khanh.toan.tran@wemove.com>
2022-11-14 11:26:43 +0100

    Sync baw_mis changes to baw_doi

--- distribution/src/profiles/baw_doi/conf/mapping/igc_to_idf_nokis.js
+++ distribution/src/profiles/baw_doi/conf/mapping/igc_to_idf_nokis.js
@@ -69,7 +69,7 @@ function addMeasureData() {
     var dataIdent = XPATH.getNode(idfDoc, "//gmd:identificationInfo/gmd:MD_DataIdentification");
     var measureInfo = DOM.addElement(dataIdent, "measurementInfo");
 
-    addItemsToDom(objId, measureInfo.addElement("MeasurementMethod"), "measurementMethod", "measuringMethod", ["measuringMethod"], ["measurementMethod"], true)
+    addItemsToDom(objId, measureInfo, "MeasurementMethod", "measuringMethod", ["measuringMethod"], ["measurementMethod"], false)
     measureInfo.addElement("spatialOrientation").addText(getAdditionalFieldFromObject(objId, "spatiality", "data"));
     measureInfo.addElement("MeasurementDepth")
         .addElement("depth").addText(getAdditionalFieldFromObject(objId, "measuringDepth", "data"))
@@ -78,14 +78,20 @@ function addMeasureData() {
         .getParent()
         .addElement("verticalCRS").addText(getAdditionalFieldFromObject(objId, "heightReferenceSystem", "data"));
     measureInfo.addElement("measurementFrequency").addText(getAdditionalFieldFromObject(objId, "measuringFrequency", "data"));
-    addItemsToDom(objId, measureInfo.addElement("MeanWaterLevel"), "meanWaterLevel", "averageWaterLevel", ["waterLevel", "unitOfMeasurement"], ["waterLevel", "uom"]);
-    addItemsToDom(objId, measureInfo.addElement("GaugeDatum"), "gaugeDatum", "zeroLevel", ["zeroLevel", "unitOfMeasurement", "verticalCoordinateReferenceSystem", "description"], ["datum", "uom", "verticalCRS", "description"]);
+    addItemsToDom(objId, measureInfo, "MeanWaterLevel", "averageWaterLevel", ["waterLevel", "unitOfMeasurement"], ["waterLevel", "uom"]);
+    addItemsToDom(objId, measureInfo, "GaugeDatum", "zeroLevel", ["zeroLevel", "unitOfMeasurement", "verticalCoordinateReferenceSystem", "description"], ["datum", "uom", "verticalCRS", "description"]);
     measureInfo.addElement("minDischarge").addText(getAdditionalFieldFromObject(objId, "drainMin", "data"));
     measureInfo.addElement("maxDischarge").addText(getAdditionalFieldFromObject(objId, "drainMax", "data"));
-    addItemsToDom(objId, measureInfo.addElement("MeasurementDevice"), "measurementDevice", "gauge", ["name", "id", "model", "description"], ["name", "id", "model", "description"]);
-    addItemsToDom(objId, measureInfo.addElement("MeasuredQuantities"), "measuredQuantities", "targetParameters", ["name", "type", "unitOfMeasurement", "formula"], ["name", "type", "uom", "calculationFormula"]);
+    addItemsToDom(objId, measureInfo, "MeasurementDevice", "gauge", ["name", "id", "model", "description"], ["name", "id", "model", "description"]);
+    addItemsToDom(objId, measureInfo, "MeasuredQuantities", "targetParameters", ["name", "type", "unitOfMeasurement", "formula"], ["name", "type", "uom", "calculationFormula"], false, [null, 3950014, null, null]);
     measureInfo.addElement("dataQualityDescription").addText(getAdditionalFieldFromObject(objId, "dataQualityDescription", "data"));
 
+}
+
+function convertToISODate(date) {
+    // add UTC to date to extract the correct date without timezone issues
+    var isoDate = new Date(date.split('.').reverse() + " UTC");
+    return isoDate.toISOString().split('T')[0];
 }
 
 function addSoftwareData() {
@@ -103,7 +109,16 @@ function addSoftwareData() {
     produktiverEinsatz.addElement("wsvAuftrag").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "productiveUseWSVContract", "data"));
     produktiverEinsatz.addElement("fUndE").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "productiveUseFuE", "data"));
     produktiverEinsatz.addElement("andere").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "productiveUseOther", "data"));
-    produktiverEinsatz.addElement("wsvAuftrag").addElement("anmerkungen").addText(getAdditionalFieldFromObject(objId, "productiveUseNotes", "data"));
+    produktiverEinsatz.addElement("anmerkungen").addText(getAdditionalFieldFromObject(objId, "productiveUseNotes", "data"));
+
+    var versionsFromDB = SQL.all("SELECT t011_version.version_value FROM t01_object t01, t011_obj_serv t011, t011_obj_serv_version t011_version WHERE t01.id=? AND t011.obj_id = t01.id AND t011_version.obj_serv_id = t011.id", [+objId]);
+    if (versionsFromDB.length > 0) {
+        for (var i=0; i<versionsFromDB.size(); i++) {
+            var version = software.addElement("Version");
+            var versionItem = versionsFromDB.get(i);
+            version.addElement("version").addText(versionItem.get("version_value"));
+        }
+    }    
 
     var ergaenzungsModul = software.addElement("ErgaenzungsModul");
     ergaenzungsModul.addElement("ergaenzungsModul").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "hasSupplementaryModule", "data"));
@@ -114,45 +129,56 @@ function addSoftwareData() {
     betriebssystem.addElement("linux").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "operatingSystemLinux", "data"));
     betriebssystem.addElement("anmerkungen").addText(getAdditionalFieldFromObject(objId, "operatingSystemNotes", "data"));
 
-    var language = software.addElement("Programmiersprache");
-    addItemsToDom(objId, language, "programmiersprache", "programmingLanguage", ["programmingLanguage"], ["programmiersprache"], true);
-    var ide = software.addElement("Entwicklungsumgebung");
-    addItemsToDom(objId, ide, "entwicklungsumgebung", "developmentEnvironment", ["developmentEnvironment"], ["entwicklungsumgebung"], true);
-    var libs = software.addElement("Bibliotheken")
-    addItemsToDom(objId, libs, "bibliothek", "libraries", ["library"], null, true);
+    // var language = software.addElement("Programmiersprache");
+    addItemsToDom(objId, software, "Programmiersprache", "programmingLanguage", ["programmingLanguage"], ["programmiersprache"], false);
+    addItemsToDom(objId, software, "Entwicklungsumgebung", "developmentEnvironment", ["developmentEnvironment"], ["entwicklungsumgebung"], false);
+    addItemsToDom(objId, software, "Bibliotheken", "libraries", ["library"], ["library"], false);
     software.addElement("Erstellungsvertrag")
         .addElement("vertragsNummer").addText(getAdditionalFieldFromObject(objId, "creationContractNumber", "data"))
         .getParent()
-        .addElement("datum").addText(getAdditionalFieldFromObject(objId, "creationContractDate", "data"));
+        .addElement("datum").addText(convertToISODate(getAdditionalFieldFromObject(objId, "creationContractDate", "data")));
 
     software.addElement("Supportvertrag")
         .addElement("vertragsNummer").addText(getAdditionalFieldFromObject(objId, "supportContractNumber", "data"))
         .getParent()
-        .addElement("datum").addText(getAdditionalFieldFromObject(objId, "supportContractDate", "data"));
+        .addElement("datum").addText(convertToISODate(getAdditionalFieldFromObject(objId, "supportContractDate", "data")))
+        .getParent()
+        .addElement("anmerkungen").addText(getAdditionalFieldFromObject(objId, "supportContractNotes", "data"));
 
     var installationsort = software.addElement("Installationsort");
     installationsort.addElement("lokal").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "installationLocal", "data"));
     var hlr = installationsort.addElement("HLR");
     hlr.addElement("hlr").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "installationHLR", "data"));
-    addItemsToDom(objId, hlr, "hlrName", "installationServerNames", ["text"], null, true);
+    addItemsToDom(objId, hlr, "hlrName", "installationServerNames", ["text"], ["text"], false);
 
     // installationsort.addElement("Server").addText(getAdditionalFieldFromObject(objId, "operatingSystemNotes", "data"));
     var server = installationsort.addElement("Server");
     server.addElement("server").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "installationServer", "data"));
-    addItemsToDom(objId, server, "servername", "installationServerNames", ["text"], null, true);
+    addItemsToDom(objId, server, "servername", "installationServerNames", ["text"], ["text"], false);
     software.addElement("installationsMethode").addText(getAdditionalFieldFromObject(objId, "installBy", "data"));
 
-    var bawRechte = software.addElement("BawRechte");
-    bawRechte.addElement("bawRights").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "hasSourceRights", "data"));
+    var bawRechte = software.addElement("QuellCodeRechte");
+    bawRechte.addElement("baw").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "hasSourceRights", "data"));
     bawRechte.addElement("anmerkungen").addText(getAdditionalFieldFromObject(objId, "sourceRightsNotes", "data"));
 
-    var subunternehmensrechte = software.addElement("Subunternehmensrechte");
-    subunternehmensrechte.addElement("bawRights").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "hasUsageRights", "data"));
+    var subunternehmensrechte = software.addElement("NutzungsRechte");
+    subunternehmensrechte.addElement("dritte").addElement("gco:Boolean").addText(getAdditionalFieldFromObject(objId, "hasUsageRights", "data"));
     subunternehmensrechte.addElement("anmerkungen").addText(getAdditionalFieldFromObject(objId, "usageRightsNotes", "data"));
 
 }
 
-function addItemsToDom(objId, targetElement, domElementId, tableId, columnIds, domColumnIds, /*boolean*/mapDirectly) {
+/**
+ * 
+ * @param objId
+ * @param targetElement
+ * @param domElementId
+ * @param tableId
+ * @param columnIds
+ * @param domColumnIds
+ * @param {boolean} [mapDirectly] this is not used anymore!
+ * @param {number[]} [mapCodelistColumn]
+ */
+function addItemsToDom(objId, targetElement, domElementId, tableId, columnIds, domColumnIds, mapDirectly, mapCodelistColumn) {
     var columns = [];
     for (var i = 0; i < columnIds.length; i++) {
         columns.push(getValuesFromTable(objId, tableId, columnIds[i]));
@@ -160,10 +186,13 @@ function addItemsToDom(objId, targetElement, domElementId, tableId, columnIds, d
     for (var i = 0; i < columns[0].length; i++) {
         var element = DOM.createElement(domElementId);
         for (var j = 0; j < columnIds.length; j++) {
+            
+            var value = columns[j][i];
+            if (mapCodelistColumn && mapCodelistColumn[j]) value = TRANSF.getISOCodeListEntryFromIGCSyslistEntry(mapCodelistColumn[j], value)
             if (mapDirectly) {
-                element.addText(columns[j][i]);
+                element.addText(value);
             } else {
-                element.addElement(domColumnIds[j]).addText(columns[j][i]);
+                element.addElement(domColumnIds[j]).addText(value);
             }
         }
         targetElement.addElement(element)
@@ -175,7 +204,10 @@ function getValuesFromTable(objId, tableId, columnId) {
     var result = [];
     for (var i = 0; i < table.length; i++) {
         if (table[i][columnId]) {
-            result.push(table[i][columnId].data);
+            var value = table[i][columnId].data;
+            if (!value && table[i][columnId].listId !== "-1") value = table[i][columnId].listId;
+            log.info("COLUMN VALUE: " + JSON.stringify(table[i][columnId]) + " using value: " + value);
+            result.push(value);
         } else {
             result.push("");
         }
@@ -193,4 +225,4 @@ function getAdditionalFieldFromObject(objId, fieldId, property) {
         }
      }
      return null;
- }
+}
