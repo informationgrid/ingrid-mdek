4a078cd8135f02d170637164db54e7448445adee
Erik-Lân Do Dinh <eriklan.dodinh@wemove.com>
2022-11-15 11:29:28 +0100

    LUBW: Write additional fields to index (#4378)

---
+++ distribution/src/profiles/lubw/conf/mapping/igc_to_lucene_lubw.js
@@ -0,0 +1,55 @@
+/*
+ * **************************************************-
+ * InGrid-iPlug DSC
+ * ==================================================
+ * Copyright (C) 2014 - 2022 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * **************************************************#
+ */
+if (javaVersion.indexOf( "1.8" ) === 0) {
+    load("nashorn:mozilla_compat.js");
+}
+
+importPackage(Packages.org.apache.lucene.document);
+importPackage(Packages.de.ingrid.iplug.dsc.om);
+importPackage(Packages.de.ingrid.geo.utils.transformation);
+
+if (log.isDebugEnabled()) {
+    log.debug("Mapping source record to lucene document: " + sourceRecord.toString());
+}
+
+if (!(sourceRecord instanceof DatabaseSourceRecord)) {
+    throw new IllegalArgumentException("Record is no DatabaseRecord!");
+}
+
+// convert id to number to be used in PreparedStatement as Integer to avoid postgres error !
+var objId = +sourceRecord.get("id");
+
+// ---- LUBW specific fields ----
+var lubwFields = ["environmentDescription", "oac"];
+var addnFieldRows = SQL.all("SELECT * FROM additional_field_data WHERE obj_id = ?", [objId]);
+for (var i = 0; i < addnFieldRows.size(); i++) {
+    var row = addnFieldRows.get(i);
+    var fieldKey = row.get("field_key");
+    var data = row.get("data");
+    for (var j = 0; j < lubwFields.length; j++) {
+        var lubwField = lubwFields[j];
+        if (fieldKey == lubwField) {
+            IDX.add(fieldKey, data);
+        }
+    }
+}
