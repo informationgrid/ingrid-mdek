192efe9aadfb43aac46e8e8265a604c7f13e074f
Toan Tran <khanh.toan.tran@wemove.com>
2023-04-04 09:48:49 +0200

    Add external map link (#4680)

--- distribution/src/profiles/hmdk/conf/mapping/igc_to_idf-hmdk.js
+++ distribution/src/profiles/hmdk/conf/mapping/igc_to_idf-hmdk.js
@@ -130,3 +130,91 @@ if ( contentLabel && contentLabel.size() > 0) {
         keywords.addElement("gmd:keyword/gco:CharacterString").addText(contentLabel.get(i).get("data"));
     }
 }
+
+var mdMetadata = DOM.getElement(idfDoc, "idf:idfMdMetadata");
+
+var objRows = SQL.all("SELECT * FROM t01_object WHERE id=?", [+id]);
+for (i=0; i<objRows.size(); i++) {
+    
+    var internetMapUrl = 'https://geofos.fhhnet.stadt.hamburg.de/fhh-atlas/?mdid=';
+    var intranetMapUrl = 'https://geoportal-hamburg.de/geo-online/?mdid=';
+
+    var objUuid = objRows.get(i).get("obj_uuid");
+    var publishId = objRows.get(i).get("publish_id");
+    var objClass = objRows.get(i).get("obj_class");
+    var objUuid = objRows.get(i).get("obj_uuid");
+
+    var hasMapUrl = false;
+
+    if (objClass == "1") {
+        var serviceObjects = SQL.all("SELECT * FROM object_reference oRef, t01_object t01 WHERE oRef.obj_to_uuid=? AND oRef.obj_to_uuid=t01.obj_uuid AND t01.obj_class=1 AND oRef.obj_from_id IN (SELECT t01_b.id FROM t01_object t01_b WHERE t01_b.obj_class=3)", [objUuid]);
+            log.debug("Found ServiceObjects from uuid=" + objUuid + ": " + serviceObjects.size());
+            for (k=0; k<serviceObjects.size(); k++) {
+                // get capabilities urls from service object, who links to this object!
+                var capabilitiesUrls = SQL.all("SELECT * FROM object_reference oref, t01_object t01obj, t011_obj_serv serv, t011_obj_serv_operation servOp, t011_Obj_serv_op_connPoint servOpConn WHERE oref.obj_from_id=t01obj.id AND serv.obj_id=t01obj.id AND servOp.obj_serv_id=serv.id AND servOp.name_key=1 AND servOpConn.obj_serv_op_id=servOp.id AND obj_to_uuid=? AND obj_from_id=? AND special_ref=3600 AND serv.type_key=2 AND t01obj.work_state='V'", [objUuid, +serviceObjects.get(k).get("obj_from_id")]);
+                for (l=0; l<capabilitiesUrls.size(); l++) {
+                    hasMapUrl = addCapabilitiesUrl(capabilitiesUrls.get(l), objUuid, publishId);
+                }
+            }
+        }
+
+        if (!hasMapUrl) {
+            var rows = SQL.all("SELECT * FROM t011_obj_serv WHERE obj_id=?", [+id]);
+            for (j=0; j<rows.size(); j++) {
+                var objServId = rows.get(j).get("id");
+                // ---------- t011_obj_serv_operation ----------
+                var subRows = SQL.all("SELECT * FROM t011_obj_serv_operation WHERE obj_serv_id=?", [+objServId]);
+                for (k=0; k<subRows.size(); k++) {
+                // ---------- t011_obj_serv_operation ----------
+                var subRows = SQL.all("SELECT * FROM t011_obj_serv_operation WHERE obj_serv_id=?", [+objServId]);
+                for (k=0; k<subRows.size(); k++) {
+                    addT011ObjServOperation(subRows.get(k));
+                    var objServOpId   = subRows.get(k).get("id");
+                    var isCapabilityOperation = rows.get(j).get("type_key") == "2" && subRows.get(k).get("name_key") == "1"; // key of "Darstellungsdienste" is "2" and "GetCapabilities" is "1" !
+
+                    // ---------- t011_obj_serv_op_connpoint ----------
+                    var subSubRows = SQL.all("SELECT * FROM t011_obj_serv_op_connpoint WHERE obj_serv_op_id=?", [+objServOpId]);
+                    for (l=0; l<subSubRows.size(); l++) {
+                        if(isCapabilityOperation) {
+                            addCapabilitiesUrl(row, objUuid, publishId);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function addCapabilitiesUrl(row, objUuid, publishId) {
+    if (!hasValue(row.get("has_access_constraint")) || row.get("has_access_constraint") !== 'Y') {
+        var externalMapUrl = intranetMapUrl;
+        if (hasValue(publishId)) {
+            if (publishId === '2') {
+                externalMapUrl = intranetMapUrl + '' + objUuid;
+            } else {
+                externalMapUrl += objUuid;
+            }
+        } else {
+            externalMapUrl += objUuid;
+        }
+        if (externalMapUrl !== intranetMapUrl) {
+            mdMetadata.addElement("idf:mapUrl").addText(value);
+            return true;
+        }
+    }
+    return false;
+}
+
+function hasValue(val) {
+    if (typeof val == "undefined") {
+        return false;
+    } else if (val == null) {
+        return false;
+    } else if (typeof val == "string" && val == "") {
+        return false;
+    } else if (typeof val == "object" && Object.keys(val).length === 0) {
+        return false;
+    } else {
+      return true;
+    }
+}
