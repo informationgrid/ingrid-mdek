c81496a07f058f83fc89c2277b43f827e0687309
Toan Tran <khanh.toan.tran@wemove.com>
2023-04-04 13:11:11 +0200

    Optimize external map link (#4680)

--- distribution/src/profiles/hmdk/conf/mapping/igc_to_lucene-hmdk.js
+++ distribution/src/profiles/hmdk/conf/mapping/igc_to_lucene-hmdk.js
@@ -42,38 +42,56 @@ for (i=0; i<objRows.size(); i++) {
     var objClass = objRows.get(i).get("obj_class");
     var objUuid = objRows.get(i).get("obj_uuid");
 
-    var hasMapUrl = false;
+    var mapUrl = '';
 
     if (objClass == "1") {
         var serviceObjects = SQL.all("SELECT * FROM object_reference oRef, t01_object t01 WHERE oRef.obj_to_uuid=? AND oRef.obj_to_uuid=t01.obj_uuid AND t01.obj_class=1 AND oRef.obj_from_id IN (SELECT t01_b.id FROM t01_object t01_b WHERE t01_b.obj_class=3)", [objUuid]);
         log.debug("Found ServiceObjects from uuid=" + objUuid + ": " + serviceObjects.size());
         for (k=0; k<serviceObjects.size(); k++) {
             // get capabilities urls from service object, who links to this object!
-            var capabilitiesUrls = SQL.all("SELECT * FROM object_reference oref, t01_object t01obj, t011_obj_serv serv, t011_obj_serv_operation servOp, t011_Obj_serv_op_connPoint servOpConn WHERE oref.obj_from_id=t01obj.id AND serv.obj_id=t01obj.id AND servOp.obj_serv_id=serv.id AND servOp.name_key=1 AND servOpConn.obj_serv_op_id=servOp.id AND obj_to_uuid=? AND obj_from_id=? AND special_ref=3600 AND serv.type_key=2 AND t01obj.work_state='V'", [objUuid, +serviceObjects.get(k).get("obj_from_id")]);
+            var objFromId = serviceObjects.get(k).get("obj_from_id");
+            var capabilitiesUrls = SQL.all("SELECT * FROM object_reference oref, t01_object t01obj, t011_obj_serv serv, t011_obj_serv_operation servOp, t011_Obj_serv_op_connPoint servOpConn WHERE oref.obj_from_id=t01obj.id AND serv.obj_id=t01obj.id AND servOp.obj_serv_id=serv.id AND servOp.name_key=1 AND servOpConn.obj_serv_op_id=servOp.id AND obj_to_uuid=? AND obj_from_id=? AND special_ref=3600 AND serv.type_key=2 AND t01obj.work_state='V'", [objUuid, +objFromId]);
             for (l=0; l<capabilitiesUrls.size(); l++) {
-                hasMapUrl = addCapabilitiesUrl(capabilitiesUrls.get(l), objUuid, publishId);
-            }
-        }
-
-        if (!hasMapUrl) {
-            var rows = SQL.all("SELECT * FROM t011_obj_serv WHERE obj_id=?", [+objId]);
-            for (j=0; j<rows.size(); j++) {
-                var objServId = rows.get(j).get("id");
-                var subRows = SQL.all("SELECT * FROM t011_obj_serv_operation WHERE obj_serv_id=?", [+objServId]);
-                for (k=0; k<subRows.size(); k++) {
-                    var objServOpId   = subRows.get(k).get("id");
-                    var isCapabilityOperation = rows.get(j).get("type_key") == "2" && subRows.get(k).get("name_key") == "1"; // key of "Darstellungsdienste" is "2" and "GetCapabilities" is "1" !
-
-                    // ---------- t011_obj_serv_op_connpoint ----------
-                    var subSubRows = SQL.all("SELECT * FROM t011_obj_serv_op_connpoint WHERE obj_serv_op_id=?", [+objServOpId]);
-                    for (l=0; l<subSubRows.size(); l++) {
-                        if(isCapabilityOperation) {
-                            addCapabilitiesUrl(subSubRows.get(l), objUuid, publishId);
+                var objFromIdRow = SQL.first("SELECT obj_uuid FROM t01_object WHERE id=?", [+objFromId]);
+                if (hasValue(objFromIdRow)) {
+                    var objFromIdUuid = objFromIdRow.get("obj_uuid");
+                    if (hasValue(objFromIdUuid)) {
+                        mapUrl = addCapabilitiesUrl(capabilitiesUrls.get(l), objFromIdUuid, publishId);
+                        if(hasValue(mapUrl)) {
+                            log.info('Add external map url to lucene by object_reference.');
+                            break;
                         }
                     }
                 }
             }
         }
+    }
+    if (!hasValue(mapUrl)) {
+        var rows = SQL.all("SELECT * FROM t011_obj_serv WHERE obj_id=?", [+objId]);
+        for (j=0; j<rows.size(); j++) {
+            var objServId = rows.get(j).get("id");
+            var subRows = SQL.all("SELECT * FROM t011_obj_serv_operation WHERE obj_serv_id=?", [+objServId]);
+            for (k=0; k<subRows.size(); k++) {
+                var objServOpId   = subRows.get(k).get("id");
+                var isCapabilityOperation = rows.get(j).get("type_key") == "2" && subRows.get(k).get("name_key") == "1"; // key of "Darstellungsdienste" is "2" and "GetCapabilities" is "1" !
+
+                // ---------- t011_obj_serv_op_connpoint ----------
+                var subSubRows = SQL.all("SELECT * FROM t011_obj_serv_op_connpoint WHERE obj_serv_op_id=?", [+objServOpId]);
+                for (l=0; l<subSubRows.size(); l++) {
+                    if(isCapabilityOperation) {
+                        mapUrl = addCapabilitiesUrl(subSubRows.get(l), objUuid, publishId);
+                        if(hasValue(mapUrl)) {
+                            log.info('Add external map url to lucene by t011_obj_serv_op_connpoint.');
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (hasValue(mapUrl)) {
+        log.info('Add external map url to lucene: ' + mapUrl);
+        IDX.add("capabilities_url_with_client", mapUrl);
     }
 }
 
@@ -92,12 +110,10 @@ function addCapabilitiesUrl(row, objUuid, publishId) {
             externalMapUrl += objUuid;
         }
         if (externalMapUrl !== internetMapUrl) {
-            log.debug('Add external map url:' + externalMapUrl);
-            IDX.add("capabilities_url_with_client", externalMapUrl);
-            return true;
+            return externalMapUrl;
         }
     }
-    return false;
+    return '';
 }
 
 function hasValue(val) {
